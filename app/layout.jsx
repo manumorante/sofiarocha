c2e77d9a2221cac017c6d53f52293280edc9a21f
Manu Morante <manu@estadologico.com>
2023-03-18 00:55:23 +0100

    Home new logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,18 +5,22 @@ import cx from 'clsx'
 const quicksand = Quicksand({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function RootLayout({ children }) {
+  const htmlCx = cx('h-full', quicksand.className)
+
+  const bodyCx = cx(
+    'Body',
+    'h-full',
+    //
+    'bg-cover',
+    'bg-center',
+    `bg-[url('/papers.jpg')]`
+  )
   return (
-    <html
-      lang='es'
-      className={cx(
-        quicksand.className,
-        'bg-white text-darkend',
-        'dark:bg-darkend dark:text-lightend'
-      )}>
+    <html lang='es' className={htmlCx} style={{ backgroundColor: '#d0ceca' }}>
       <head>
         <meta property='og:image' content='/public/OG.jpg' />
       </head>
-      {children}
+      <body className={bodyCx}>{children}</body>
     </html>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,21 @@
-export default function Page() {
-  return (
-    <div className='Page'>
-      <h1 className='font-extrabold text-2xl'>Sofía Rocha</h1>
+import cx from 'clsx'
+import Image from 'next/image'
+import { Logo } from 'components/Logo'
 
-      <p className='text-gray-500'>
-        Web personal
-      </p>
+export default function Home() {
+  const mainCx = cx('w-full h-full flex items-center justify-center')
+  const cardCx = cx(
+    'relative aspect-card p-8 text-[#6b6451] bg-[#faf5f4] flex items-center justify-center shadow-xl'
+  )
+  const paperCx = cx('absolute z-0 w-full h-full object-cover')
+  const logoCx = cx('relative z-10')
+
+  return (
+    <div className={mainCx}>
+      <div className={cardCx}>
+        <Logo className={logoCx} />
+        <Image className={paperCx} width={384} height={224} src='/paper.jpg' alt='Paper' />
+      </div>
     </div>
   )
 }
