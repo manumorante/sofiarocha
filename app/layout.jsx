bc264614453b77a58cb4657b598931f8f46a3afa
Manu Morante <manu@estadologico.com>
2023-03-18 02:15:38 +0100

    Critical htmlStyles and bodyStyles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,35 +1,22 @@
 import 'styles/globals.css'
 
-import cx from 'clsx'
+const htmlStyles = { height: '100%', backgroundColor: '#d0ceca' }
+const bodyStyles = { height: '100%' }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='es'>
+    <html lang='es' style={htmlStyles}>
       <head>
         <title>Sofía Rocha - masajista profesional</title>
-        <style
-          dangerouslySetInnerHTML={{
-            __html: `
-                html, body {
-                  height: 100%;
-                  background-color: '#d0ceca'
-                }                
-              `,
-          }}
-        />
+        <meta name='description' content='Sofía Rocha - masajista profesional' />
 
-        <meta
-          name='description'
-          content='Sofía Rocha - masajista profesional, masajes relajantes y terapéuticos para mejorar tu bienestar. Reserva una cita.'
-        />
         <link rel='icon' href='/favicon.ico' />
-
         <meta property='og:title' content='Sofía Rocha' />
         <meta property='og:image' content='/og/sofia-rocha-masajista.jpg' />
         <meta property='og:image:width' content='1200' />
         <meta property='og:image:height' content='630' />
       </head>
-      <body>{children}</body>
+      <body style={bodyStyles}>{children}</body>
     </html>
   )
 }
