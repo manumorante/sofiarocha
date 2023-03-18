0f39a443ae0b4a71a5fcd34f4dae9063d0f6bb73
Manu Morante <manu@estadologico.com>
2023-03-18 01:07:44 +0100

    Scale logo for iPhone X

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,17 @@ import Image from 'next/image'
 import { Logo } from 'components/Logo'
 
 export default function Home() {
-  const mainCx = cx('w-full h-full flex items-center justify-center')
+  const mainCx = cx('Home w-full h-full flex items-center justify-center')
   const cardCx = cx(
-    'relative aspect-card p-8 text-[#6b6451] bg-[#faf5f4] flex items-center justify-center shadow-xl'
+    'Card',
+    'relative',
+    'aspect-card p-8 scale-75 sm:scale-100',
+    'flex items-center justify-center',
+    'text-[#6b6451] bg-[#faf5f4]',
+    'shadow-xl'
   )
-  const paperCx = cx('absolute z-0 w-full h-full object-cover')
-  const logoCx = cx('relative z-10')
+  const paperCx = cx('Paper absolute z-0 w-full h-full object-cover')
+  const logoCx = cx('Logo relative z-10')
 
   return (
     <div className={mainCx}>
